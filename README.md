35a3a1db1a8eafb16f6c54161640188604a5f7ad
Tim Kelly <tim.kelly@4mation.com.au>
2021-05-14 11:36:32 +1000

    Created new repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Laptop Setup
+
+A simple `run.sh` to set up my favourite visual studio code extensions on a new computer.
---
+++ run.sh
@@ -0,0 +1,37 @@
+# Install Visual Studio Code Extensions
+
+## https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+code --install-extension aaron-bond.better-comments
+
+## https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer-2
+code --install-extension coenraads.bracket-pair-colorizer-2
+
+## https://marketplace.visualstudio.com/items?itemName=Tyriar.lorem-ipsum
+code --install-extension tyriar.lorem-ipsum
+
+## https://marketplace.visualstudio.com/items?itemName=DavidAnson.vscode-markdownlint
+code --install-extension tyriar.davidanson.vscode-markdownlint
+
+## https://marketplace.visualstudio.com/items?itemName=Syler.sass-indented
+code --install-extension syler.sass-indented
+
+## https://marketplace.visualstudio.com/items?itemName=cssho.vscode-svgviewer
+code --install-extension cssho.vscode-svgviewer
+
+## https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+code --install-extension bradlc.vscode-tailwindcss
+
+## https://marketplace.visualstudio.com/items?itemName=vscode-icons-team.vscode-icons
+code --install-extension vscode-icons-team.vscode-icons
+
+## https://marketplace.visualstudio.com/items?itemName=jpoissonnier.vscode-styled-components
+code --install-extension jpoissonnier.vscode-styled-components
+
+## https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+code --install-extension dbaeumer.vscode-eslint
+
+## https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+code --install-extension esbenp.prettier-vscode
+
+## https://marketplace.visualstudio.com/items?itemName=Huuums.vscode-fast-folder-structure
+code --install-extension huuums.vscode-fast-folder-structure
